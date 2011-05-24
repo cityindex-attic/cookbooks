659d97a0f6f633b766d089dc19ac71f89f2556d8
Ryan Geyer <me@ryangeyer.com>
2011-05-23 17:28:20 -0700

    Clear up our conflicts with unicorn

--- cookbooks/shapado/recipes/default.rb
+++ cookbooks/shapado/recipes/default.rb
@@ -22,15 +22,6 @@ if(node[:shapado][:recaptcha_enable] && (!node[:shapado][:recaptcha_public_key] 
   raise "Recaptcha support for shapado was enabled, but the public or private API key was empty"
 end
 
-# TODO: This mechanism for getting the arch is reused in my code a lot, need to either set a node attribute once
-# somewhere, or otherwise encapsulate this, maybe ohai?
-#uname_machine = `uname -m`.strip
-#
-#machines = {"x86_64" => "amd64", "i386" => "i386", "i686" => "i386"}
-#arch = machines[uname_machine]
-#
-#Chef::Log.info "Detected system architecture of #{uname_machine} installing the #{arch} RVM gemset for Shapado..."
-
 # Required for the "magic" gem
 package "file"
 
@@ -51,17 +42,6 @@ else
   end
 end
 
-# TODO: This is a total hack, and this should become a distro package at some point
-#ree_global_gemset_path = ::File.join(node[:rvm][:install_path], "gems", "ree-#{node[:ruby_enterprise][:version]}")
-#
-#remote_file "/tmp/gemset.tar.gz" do
-#  source "gemset-shapado-#{node[:shapado][:version]}-#{arch}.tar.gz"
-#end
-#
-#bash "Extract gemset to ree global gemset" do
-#  code "tar -zxf /tmp/gemset.tar.gz -C #{ree_global_gemset_path}"
-#end
-
 # TODO: Lots of good configuration bits like enabling social networking and google analytics
 template ::File.join(shapado_install_dir, "config", "shapado.yml") do
   source "shapado.yml.erb"
@@ -71,21 +51,9 @@ end
 bash "Install gems & bootstrap shapado" do
   cwd ::File.join(node[:nginx][:content_dir], "shapado")
   code <<-EOF
-#rvm_script="#{node[:rvm][:install_path]}/scripts/rvm"
-#echo "Testing for RVM using $rvm_script"
-#if [ ! -f $rvm_script ]
-#then
-#  echo "No RVM installation found, not loading RVM environment"
-#  exit 0
-#fi
-#
-#if [[ -s "$rvm_script" ]] ; then
-#  echo "Found a default RVM environment, loading it now"
-#  source "$rvm_script"
-#else
-#  echo "No default RVM environment found, can not continue.  Try setting one with rvm --default"
-#  exit 0
-#fi
+
+# Unicorn installs a newer version which we don't like
+gem uninstall warden
 
 rvm gemset import #{gemset_filepath}
 
