fc0089663a636cf25e11e973b665d40de3b2b993
Ryan J. Geyer <me@ryangeyer.com>
2012-01-12 15:22:54 -0800

    Newer shapado version support

--- cookbooks/shapado/metadata.rb
+++ cookbooks/shapado/metadata.rb
@@ -3,145 +3,150 @@ maintainer_email "me@ryangeyer.com"
 license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE.txt')))
 description      "Installs/Configures shapado"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.3"
 
 supports "ubuntu"
 
+%w{rvm nginx unicorn rs_utils}.each do |d|
+  depends d
+end
+
 recipe "shapado::default","Installs and configures the Shapado FAQ rails application using ruby_enterprise and unicorn"
+recipe "shapado::install_shapado","Does the same thing as default, but for later more mature versions of shapado"
 
 attribute "shapado/fqdn",
   :display_name => "Shapado VHOST FQDN",
   :description => "The fully qualified domain name (FQDN) of the new vhost to deploy shapado to.  Example www.apache.org",
   :required => true,
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "shapado/aliases",
   :display_name => "Shapado VHOST Aliases",
   :description => "The possible hostname aliases (if any) for the vhost.  For instance to host the same content at www.yourdomain.com and yourdomain.com simply put \"yourdomain.com\" here.  Many values can be supplied, separated by spaces",
   :default => "",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "shapado/recaptcha_enable",
   :display_name => "Shapado Enable Recaptcha",
   :description => "A boolean indicating of recaptcha support should be used.  If true, shapado/recaptcha_public_key and shapado/recaptcha_private_key must be set",
   :default => "false",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "shapado/recaptcha_public_key",
   :display_name => "Shapado Recaptcha Public Key",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "shapado/recaptcha_private_key",
   :display_name => "Shapado Recaptcha Private Key",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "shapado/version",
   :display_name => "Shapado Version",
   :description => "The version/branch/tag/commit of the gitorious repo to use",
   :required => "optional",
   :default => "master",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "rails/version",
   :display_name => "Rails Version",
   :description => "The full version number of rails to install.  I.E. 3.0.5  If no value is provided, the latest available version is installed.",
   :required => "optional",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "rails/environment",
   :display_name => "Rails Environment",
   :description => "The desired rails environment to run.  I.E. production",
   :required => "required",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/dir",
   :display_name => "Nginx Directory",
   :description => "Location of nginx configuration files",
   :default => "/etc/nginx",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/content_dir",
   :display_name => "Nginx Content Directory",
   :description => "Location of nginx content files",
   :default => "/var/www",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/log_dir",
   :display_name => "Nginx Log Directory",
   :description => "Location for nginx logs",
   :default => "/var/log/nginx",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/user",
   :display_name => "Nginx User",
   :description => "User nginx will run as",
   :default => "www-data",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/binary",
   :display_name => "Nginx Binary",
   :description => "Location of the nginx server binary",
   :default => "/usr/sbin/nginx",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/gzip",
   :display_name => "Nginx Gzip",
   :description => "Whether gzip is enabled",
   :default => "on",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/gzip_http_version",
   :display_name => "Nginx Gzip HTTP Version",
   :description => "Version of HTTP Gzip",
   :default => "1.0",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/gzip_comp_level",
   :display_name => "Nginx Gzip Compression Level",
   :description => "Amount of compression to use",
   :default => "2",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/gzip_proxied",
   :display_name => "Nginx Gzip Proxied",
   :description => "Whether gzip is proxied",
   :default => "any",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/gzip_types",
   :display_name => "Nginx Gzip Types",
   :description => "Supported MIME-types for gzip",
   :type => "array",
   :default => [ "text/plain", "text/html", "text/css", "application/x-javascript", "text/xml", "application/xml", "application/xml+rss", "text/javascript" ],
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/keepalive",
   :display_name => "Nginx Keepalive",
   :description => "Whether to enable keepalive",
   :default => "on",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/keepalive_timeout",
   :display_name => "Nginx Keepalive Timeout",
   :default => "65",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/worker_processes",
   :display_name => "Nginx Worker Processes",
   :description => "Number of worker processes",
   :default => "1",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/worker_connections",
   :display_name => "Nginx Worker Connections",
   :description => "Number of connections per worker",
   :default => "1024",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/server_names_hash_bucket_size",
   :display_name => "Nginx Server Names Hash Bucket Size",
   :default => "64",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 attribute "nginx/aliases",
   :display_name => "Nginx Site Aliases",
@@ -149,7 +154,7 @@ attribute "nginx/aliases",
   :type => "array",
   :default => [],
   :required => "recommended",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
 
 # HAX
 attribute "rvm/install_path",
@@ -157,4 +162,4 @@ attribute "rvm/install_path",
   :description => "The full path where RVM will be installed. I.E. /opt/rvm",
   :required => "optional",
   :default => "/opt/rvm",
-  :recipes => ["shapado::default"]
+  :recipes => ["shapado::default","shapado::install_shapado"]
---
+++ cookbooks/shapado/recipes/install_shapado.rb
@@ -0,0 +1,124 @@
+#
+# Cookbook Name:: shapado
+# Recipe:: v3.x
+#
+#  OpsCode Chef Cookbooks
+#  Copyright 2012 CityIndex Ltd.
+#
+#  This product includes software developed at
+#    CityIndex Ltd. (http://www.cityindex.com)
+#
+
+# RVM with 1.9.2 (at least) - Use rvm::default w/ ruby name == 1.9.2
+#gem update --system (Need RubyGems ~> 1.8)
+#apt-get install libqt4-dev uuid-dev
+#gem install bundler --no-ri --no-rdoc
+#bundle install
+#
+#cp config/shapado.yml.sample config/shapado.yml
+#cp config/mongoid.yml.sample config/mongoid.yml
+#cp config/auth_providers.yml.sample config/auth_providers.yml
+#
+#bundle exec rake bootstrap RAILS_ENV=production
+
+rs_utils_marker :begin
+
+node[:rvm][:ruby] = "ruby-1.9.2-p180"
+node[:rvm][:install_path] = "/opt/rvm"
+node[:unicorn][:version] = "4.1.1"
+
+include_recipe "rvm::default"
+include_recipe "nginx::default"
+include_recipe "unicorn::setup_unicorn"
+
+shapado_install_dir = ::File.join(node[:nginx][:content_dir], "shapado")
+
+if(node[:shapado][:recaptcha_enable] && (!node[:shapado][:recaptcha_public_key] || node[:shapado][:recaptcha_private_key]))
+  raise "Recaptcha support for shapado was enabled, but the public or private API key was empty"
+end
+
+# Gem native extension requirements
+%w{libqt4-dev uuid-dev}.each do |p|
+  package p
+end
+
+bash "Update RubyGems" do
+  code <<-EOF
+gem update --system
+  EOF
+end
+
+gem_package "bundler"
+
+git shapado_install_dir do
+  repository "git://gitorious.org/shapado/shapado.git"
+  revision node[:shapado][:version]
+  action :sync
+  not_if { ::File.directory?(shapado_install_dir) }
+end
+
+cookbook_file "/tmp/answer_wrapper.patch" do
+  source "answer_wrapper.patch"
+end
+
+bash "Shapado apply app/mustache/answer_wrapper.rb patch" do
+  cwd shapado_install_dir
+  code "patch -p0 </tmp/answer_wrapper.patch"
+end
+
+# TODO: Lots of good configuration bits like enabling social networking and google analytics
+template ::File.join(shapado_install_dir, "config", "shapado.yml") do
+  source "shapado.yml.erb"
+  variables(:uri => node[:shapado][:fqdn])
+end
+
+bash "Shapado gem bundle install" do
+  cwd shapado_install_dir
+  code "bundle install"
+end
+
+bash "Shapado copy config files" do
+  cwd shapado_install_dir
+  code <<-EOF
+cp config/mongoid.yml.sample config/mongoid.yml
+cp config/auth_providers.yml.sample config/auth_providers.yml
+  EOF
+end
+
+bash "Shapado update GeoIP data" do
+  cwd shapado_install_dir
+  code "script/update_geoip"
+end
+
+bash "Shapado bootstrap" do
+  cwd shapado_install_dir
+  code <<-EOF
+if [ ! -f 'bootstrapped' ] ; then
+  echo "Bootstrapping shapado one time"
+  RAILS_ENV=#{node[:rails][:environment]} bundle exec rake bootstrap
+  touch bootstrapped
+else
+  echo "Shapado has already been boostrapped, skipping the bootstrap command"
+fi
+# Ignore errors/warnings from rake bootstrap, cause it complains a lot
+exit 0
+  EOF
+end
+
+unicorn_app "shapado" do
+  app_name "shapado"
+  app_path shapado_install_dir
+  cookbook "shapado"
+  template "unicorn.rb.erb"
+end
+
+nginx_enable_vhost node[:shapado][:fqdn] do
+  cookbook "shapado"
+  template "nginx.conf.erb"
+  fqdn node[:shapado][:fqdn]
+  aliases node[:shapado][:aliases]
+  shapado_path shapado_install_dir
+  create_doc_root false
+end
+
+rs_utils_marker :end
