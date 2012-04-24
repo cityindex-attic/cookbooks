a546050840670ea1494863dfec0b4a400aa24967
David Laing <david@davidlaing.com>
2012-04-24 18:35:21 +0100

    Beginnings of windows_users cookbook -> creates local windows users based on database search

---
+++ cookbooks/windows_users/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "City Index Ltd"
+maintainer_email "david.laing@cityindex.com"
+license          "Apache 2.0"
+description      "Installs/Configures windows_users"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.2"
