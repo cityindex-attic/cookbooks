f5b31eff23b1306da05cc392b717ee0c767ce462
David Laing <david@davidlaing.com>
2012-05-15 18:25:53 +0100

    Update NTPd.conf to not use local clock

--- cookbooks/windows_splunk/metadata.rb
+++ cookbooks/windows_splunk/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "david.laing@cityindex.com"
 license          "Apache 2.0"
 description      "Installs/Configures windows_splunk"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.8"
+version          "0.0.9"
 depends 		 "windows", ">= 1.0.0"
