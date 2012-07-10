ce60fb06172f0e6689def68a051be7b9fc22daa7
Ryan J. Geyer <me@ryangeyer.com>
2012-07-10 14:58:43 -0700

    Can't refer to the license file which is outside of the cookbook directories, else chef 0.10.x attempts to read it and raises an exception

--- cookbooks/shapado/metadata.rb
+++ cookbooks/shapado/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "CityIndex Ltd."
 maintainer_email "me@ryangeyer.com"
-license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE.txt')))
+license          "Apache 2.0"
 description      "Installs/Configures shapado"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.3"
